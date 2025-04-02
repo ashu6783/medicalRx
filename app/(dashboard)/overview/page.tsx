cf3f58360690cfa55508b47afc68e2d65da37509
ashutosh gaurav <ashu00665@gmail.com>
2025-04-03 00:27:08 +0530

    Remove Overview component from dashboard

--- app/(dashboard)/overview/page.tsx
+++
@@ -1,9 +0,0 @@
-// "use-client"
-
-// export default function Overview() {
-//     return (
-//         <div>
-//             <h1 className="text-3xl text-black">Overview</h1>
-//         </div>
-//     )
-// }
