c5667f316ab49113a79389f72f1b3f1414b12783
ashutosh gaurav <ashu00665@gmail.com>
2025-04-05 15:52:47 +0530

    fix: change time field type from Date to string in Reminder model

--- components/modals/Reminder.ts
+++ components/modals/Reminder.ts
@@ -2,12 +2,12 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface IReminder extends Document {
   text: string;
-  time: Date;
+  time: string;
 }
 
 const ReminderSchema: Schema = new Schema({
   text: { type: String, required: true },
-  time: { type: Date, required: true },
+  time: { type: String, required: true },
 });
 
 const Reminder = mongoose.models.Reminder || mongoose.model<IReminder>("Reminder", ReminderSchema);
